2208b997dbdc7a65d7c385b3b2ba811e913dc066
Vard <vardgalstyan99@gmail.com>
2024-03-04 22:44:48 +0400

    Added sas

--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -1,51 +1,51 @@
 import ButtonForSection from "../../common/button";
 import { NavLink } from "react-router-dom";
-import "./style.css";
-import { ROUTES } from "../../utils/constants";
 
+import { ROUTES } from "../../utils/constants";
+import css from "./styled.module.scss"
 const AboutUs = () => {
   return (
-    <div className="about-us-section">
-      <div className="about-section">
-        <h1 className="about-title">About Us</h1>
-        <p className="about-description">
+    <div className={css.aboutUsSection}>
+      <div className={css.aboutSection}>
+        <h1 className={css.aboutTitle}>About Us</h1>
+        <p className={css.aboutDescription}>
           We're big believers in the power of a good meal and a friendly place
           to eat it. Stop by for fresh, locally sourced food, served with a warm
           smile.
         </p>
       </div>
-      <div className="about-menu">
-        <div className="about-section-left"></div>
-        <div className="about-section-right">
-          <h1 className="about-title">The Best Bite in the Neighborhood</h1>
-          <p className="about-description">
+      <div className={css.aboutMenu}>
+        <div className={css.aboutSectionLeft}></div>
+        <div className={css.aboutSectionRight}>
+          <h1 className={css.aboutTitle}>The Best Bite in the Neighborhood</h1>
+          <p className={css.aboutDescription}>
             I'm an extra long title. Click here to add your own text and edit
             me. It’s easy. Just click “Edit Text” or double click me to add your
             own content and make changes to the font. I’m a great place for you
             to tell a story and let your users know a little more about you.
           </p>
-          <NavLink className="nav-link" to={ROUTES.menu}>
+          <NavLink className={css.navLink} to={ROUTES.menu}>
             <ButtonForSection title={"View Menu"} />
           </NavLink>
         </div>
       </div>
-      <div className="post">
+      <div className={css.post}>
         <h3>Real Food. No Secret Ingredients.</h3>
       </div>
-      <div className="about-service">
-        <div className="service-section-left">
-          <h1 className="service-title">From Our Hearts to Your Plate</h1>
-          <p className="service-description">
+      <div className={css.aboutService}>
+        <div className={css.serviceSectionLeft}>
+          <h1 className={css.serviceTitle}>From Our Hearts to Your Plate</h1>
+          <p className={css.serviceDescription}>
             I'm an extra long title. Click here to add your own text and edit
             me. It’s easy. Just click “Edit Text” or double click me to add your
             own content and make changes to the font. I’m a great place for you
             to tell a story and let your users know a little more about you.
           </p>
-          <NavLink className="nav-link" to={ROUTES.home}>
+          <NavLink className={css.navLink} to={ROUTES.home}>
             <ButtonForSection title={"Visit Us"} />
           </NavLink>
         </div>
-        <div className="service-section-right"></div>
+        <div className={css.serviceSectionRight}></div>
       </div>
     </div>
   );
