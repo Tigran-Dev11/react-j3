5b08770c624c66a7b9211f6da3d5787c5e12b1a7
Tigran <tigran@elitedevsquad.com>
2024-03-20 15:43:27 +0400

    using vite and yarn

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import Header from "./components/header/header";
-
 function App() {
-
-
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+  return <h1>REACT JS G3</h1>;
 }
 
 export default App;
