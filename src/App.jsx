d6462560a5905116fcafd0cc11bd128ab4fadf90
garnik200 <Gevorgyangarnik57@gmail.com>
2024-03-31 21:32:12 +0400

    #Logika

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,19 @@
-
-<<<<<<< HEAD
-import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
 import Tudulist from "./pages/Todo/tudu";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './components/login';
-=======
-
 import Footer from "./components/footer/footer";
-import Header from "./components/header/header";
 import Router from "./routes/routes";
->>>>>>> 4769f1a41189e52e0dec03f8fc132891dc229628
-
-
+import Header from "./components/header/header";
 function App() {
   return(
-<<<<<<< HEAD
-    <Router>
- <div  className=""> 
-=======
-<>
->>>>>>> 4769f1a41189e52e0dec03f8fc132891dc229628
+
+
+ <div > 
 <Header/>
 <Router/>
 <Footer/>
-<<<<<<< HEAD
 <Tudulist/>
-<Switch>
-          <Route path="/login" component={Login} />
-          
-        </Switch>
-</div>
-</Router>
-=======
-</>
 
->>>>>>> 4769f1a41189e52e0dec03f8fc132891dc229628
+</div>
+
   );
 };
 
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -40,40 +40,88 @@
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "../../utils/constants";
 
-const Login = () => {
+// const Login = () => {
 
-    const navigate = useNavigate();
-    const {
-    loginr,
-    handleSubmit,
-    formState: { errors }
-  } = useForm({
-    resolver: yupResolver(SCHEMAS.loginSchema)
-  });
-  const onSubmit = (data) => {
-       const {email,password } =data;
-  };
-  return (
-    <div className={css.container}>
-      <h1 className={css.title}>Login</h1>
-      <form className={css.loginrForm} onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="last name" {...("firstName")}/>
-         <p>{errors?.firstName?.message}</p>
-        <input type="text" placeholder="first name" {...login("lastName")}/>
-        <p>{errors?.lastName?.message}</p>
-        <input type="email" placeholder="email" {...login("email")}/>
-        <p>{errors?.email?.message}</p>
-        <input type="password" placeholder="password" {...login("password")}/>
-        <p>{errors?.password?.message}</p>
-        <input type="password" placeholder="copy password"  {...login("passwordConfirmation")}/>
-        <p>{errors?.passwordConfirmation?.message}</p>
-        <input type="checkbox" id="checkbox" {...login("isAccept")}/>
-        <label htmlFor="checkbox">Accepted privacy policy</label>
-        <p>{errors?.isAccept?.message}</p>
-        <button>login</button>
-      </form>
+//     const navigate = useNavigate();
+//     const {
+//     loginr,
+//     handleSubmit,
+//     formState: { errors }
+//   } = useForm({
+//     resolver: yupResolver(SCHEMAS.loginSchema)
+//   });
+//   const onSubmit = (data) => {
+//        const {email,password } =data;
+//   };
+//   return (
+//     <div className={css.container}>
+//       <h1 className={css.title}>Login</h1>
+//       <form className={css.loginrForm} onSubmit={handleSubmit(onSubmit)}>
+//         <input type="text" placeholder="last name" {...("firstName")}/>
+//          <p>{errors?.firstName?.message}</p>
+//         <input type="text" placeholder="first name" {...login("lastName")}/>
+//         <p>{errors?.lastName?.message}</p>
+//         <input type="email" placeholder="email" {...login("email")}/>
+//         <p>{errors?.email?.message}</p>
+//         <input type="password" placeholder="password" {...login("password")}/>
+//         <p>{errors?.password?.message}</p>
+//         <input type="password" placeholder="copy password"  {...login("passwordConfirmation")}/>
+//         <p>{errors?.passwordConfirmation?.message}</p>
+//         <input type="checkbox" id="checkbox" {...login("isAccept")}/>
+//         <label htmlFor="checkbox">Accepted privacy policy</label>
+//         <p>{errors?.isAccept?.message}</p>
+//         <button>login</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Login;
+lo
+const Login =()=>{
+  return(
+<div class="container">
+  <input type="checkbox" id="register_toggle"/>
+  <div class="slider">
+    <form class="form">
+      <span class="title">Login</span>
+      <div class="form_control">
+        <input type="text" class="input" required=""/>
+        <label class="label">Username</label>
+      </div>
+      <div class="form_control">
+        <input type="password" class="input" required=""/>
+        <label class="label">Password</label>
+      </div>
+      <button>Login</button>
+
+      <span class="bottom_text">Don't have an account? <label for="register_toggle" class="swtich">Sign Up</label> </span>
+    </form>
+    <form class="form">
+      <span class="title">Sign Up</span>
+      <div class="form_control">
+        <input type="text" class="input" required=""/>
+        <label class="label">Username</label>
+      </div>
+      <div class="form_control">
+        <input type="email" class="input" required=""/>
+        <label class="label">Email</label>
+      </div>
+      <div class="form_control">
+        <input type="password" class="input" required=""/>
+        <label class="label">Password</label>
+      </div>
+      <button>Sign Up</button>
+
+      <span class="bottom_text">Already have an account? <label for="register_toggle" class="swtich">Sign In</label> </span>
+    </form>
+
+    
     </div>
-  );
-};
+</div>
 
-export default Login;
+
+  )
+}
+
+export default Login
