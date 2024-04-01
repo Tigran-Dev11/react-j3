0f1aedc2dbc3509956ce7b64b279b4a6945872ca
VardGalstyan1 <vardgalstyan1999@gmail.com>
2024-04-01 22:20:55 +0400

    test

--- src/pages/users/edit-user/index.jsx
+++ src/pages/users/edit-user/index.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useUserActions } from "../../../hooks/use-user-actions";
 import { UseFetch } from "../../../hooks/use-fetch";
 
@@ -6,23 +6,39 @@ const EditUser = () => {
   const { "user-id": id } = useParams();
   const token =
     "4859a411b1d21f2494d65e15ec6c9f3af6b204d0e12a1ae507c88e69ea35e95b";
-
+const navigate = useNavigate()
   const options = {
     method: "GET",
     headers: {
       Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
-      Accept: "application/json"
-    }
+      Accept: "application/json",
+    },
   };
+
+
 
   const [userOne] = UseFetch({ url: `/users/${id}`, options });
 
   const { updateUser } = useUserActions();
 
-  console.log(userOne);
+const editValue =(e)=>{
+  e.preventDefault();
+  updateUser()
+navigate('/arm/user-list')
+}
+  return (
+    <div>
+      <form onSubmit={editValue} >
 
-  return <div>{id}</div>;
+        <input type="text" value={id} />
+        <input type="text"  defaultValue={userOne.name} />
+        <input type="email" defaultValue={userOne.email} />
+        <input type="text"defaultValue={userOne.gender} />
+        <button type="submit">Edit</button>
+      </form>
+    </div>
+  );
 };
 
 export default EditUser;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,27 +1,9 @@
 import { v4 as uuid } from "uuid";
 import { ROUTES } from "../utils/constants";
-import Home from "../pages/home";
-import Product from "../pages/product";
-import SingleProduct from "../pages/single-product";
 import UserList from "../pages/users/user-list";
 import EditUser from "../pages/users/edit-user";
 
 export const routes = [
-  {
-    id: uuid(),
-    path: ROUTES.home,
-    component: Home,
-  },
-  {
-    id: uuid(),
-    path: ROUTES.product,
-    component: Product,
-  },
-  {
-    id: uuid(),
-    path: ROUTES.productSingle,
-    component: SingleProduct,
-  },
   {
     id: uuid(),
     path: ROUTES.userList,
