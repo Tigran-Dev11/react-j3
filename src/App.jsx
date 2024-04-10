26314ae2d8ce428cb8799e31f66c52e6c256971e
VardGalstyan1 <vardgalstyan1999@gmail.com>
2024-04-10 14:55:24 +0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React from "react"; 
+
 import Project from "./routes/Project";
 import About from "./routes/About";
 import Contact from "./routes/Contact";
 import Login from "./routes/login";
  import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
  function App() {
   return (
 <div>
 <Routes>
-<Route path="/" element={<Hom/>}/>
-<Route path="project" element={<Project/>}/>
-<Route path="about" element={<About/>}/>
-<Route path="contact" element={<Contact/>}/>
-<Route path="login" element={<Login/>}/>
+<Route path="/" element={<Home/>}/>
+<Route path="/project" element={<Project/>}/>
+<Route path="/about" element={<About/>}/>
+<Route path="/contact" element={<Contact/>}/>
+<Route path="/login" element={<Login/>}/>
 
 </Routes>
 
